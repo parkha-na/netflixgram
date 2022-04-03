4df20dd90124a0e27ff09e5f65d47c43a4a9f2c2
FreshBread <white_len@naver.com>
2022-04-03 15:23:57 +0900

    초기 프로젝트 내용 커밋

---
+++ src/main/java/io/hana/netflixgram/NetflixgramApplication.java
@@ -0,0 +1,13 @@
+package io.hana.netflixgram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetflixgramApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NetflixgramApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/hana/netflixgram/NetflixgramApplicationTests.java
@@ -0,0 +1,13 @@
+package io.hana.netflixgram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetflixgramApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
